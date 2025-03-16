261e6bf9fe83b2793e635ce5618e139199a5f53e
sam <sanskarsharma9005@gmail.com>
2025-03-16 15:48:17 +0530

    added provder

---
+++ src/app/provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sooner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+const queryClient = new QueryClient();
+
+export function Provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        {children}
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
