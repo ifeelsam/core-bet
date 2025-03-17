7975b928c5ab248c683900c75f5001ee7e12cce4
sam <sanskarsharma9005@gmail.com>
2025-03-17 23:32:42 +0530

    add new chains

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,7 @@
 import {createConfig, http} from "wagmi"
-
+import {mainnet, coreDao} from "wagmi/chains"
 export const coreDaoTestnet = {
-  id: 1115,
+  id: 1114,
   name: 'CoreDAO Testnet',
   nativeCurrency: {
     name: 'CORE',
@@ -24,10 +24,37 @@ export const coreDaoTestnet = {
   },
   testnet: true,
 };
+export const coreDaoTestnet1  = {
+  id: 1115,
+  name: 'CoreDAO Testnet',
+  nativeCurrency: {
+    name: 'CORE',
+    symbol: 'CORE',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.test.btcs.network'],
+    },
+    public: {
+      http: ['https://rpc.test.btcs.network'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'CoreDAO Testnet Explorer',
+      url: 'https://scan.test.btcs.network',
+    },
+  },
+  testnet: true,
+};
 
 export const config = createConfig({
-    chains: [coreDaoTestnet],
+    chains: [coreDaoTestnet, mainnet, coreDao],
     transports: {
+        [mainnet.id]: http(),
+        [coreDao.id] : http(),
+        [coreDaoTestnet1.id]: http(),
         [coreDaoTestnet.id]: http()
     }
 
